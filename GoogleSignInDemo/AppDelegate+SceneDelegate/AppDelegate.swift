73317d47b967bad9cbc20dae1e588219c51d95d3
Saurav Sagar <sagarsaurav1997@gmail.com>
2022-02-13 20:32:00 +0530

    first commit

--- GoogleSignInDemo/AppDelegate+SceneDelegate/AppDelegate.swift
+++ GoogleSignInDemo/AppDelegate+SceneDelegate/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+//        GIDSignIn.sharedInstance.clientID = "54286744068-7j9a8e3fqda136jro4gtao344rftsij8.apps.googleusercontent.com"
         return true
     }
 
@@ -30,7 +34,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
 
 }
 
